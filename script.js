69bd7653f43454d434619e041764edf16751ad25
Vov4ik-md <139490040+Vov4ik-md@users.noreply.github.com>
2023-11-01 01:18:39 +0200

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 let inputTask = document.querySelector('.myinput')
 let list = document.querySelector(".mytasks")
 let ifTaskComplete = false
+//! Function to add a new task
 function newTask() {
 	let li = document.createElement('LI')
 	let inputValue = document.querySelector('.myinput').value
@@ -18,24 +20,23 @@ function newTask() {
 		}
 	}
 	document.querySelector('.myinput').value = ''
-
+//?add a close uton to a new li element
 	let span = document.createElement('SPAN')
 	let txt = document.createTextNode("\u00D7")
 	span.className = 'close'
 	span.appendChild(txt)
 	li.appendChild(span)
-
 	span.addEventListener("click", function () {
 		li.remove()
 	})
 }
-
+//press enter event
 inputTask.addEventListener('keydown', (event) => {
 	if (event.key === 'Enter') {
 		newTask()
 	}
 })
-
+//function to change element if clicked once or twice
 list.addEventListener("click",function (e) {
 	if (e.target.tagName === "LI") {
 		e.target.classList.toggle("checked")
@@ -43,7 +44,7 @@ list.addEventListener("click",function (e) {
 		}
 	}, false
 )
-
+//filter-function to show finished (checked) tasks
 function completedTasks() {
 	let added = document.querySelectorAll(".added")
 	for (let i = 0; i < added.length; i++){
@@ -55,19 +56,20 @@ function completedTasks() {
 	}
 	ifTaskComplete = true
 }
-
+//filter-function to show unfinished (unchecked) tasks
 function incompletedTasks() {
 	let completed = document.querySelectorAll('.checked')
 	for (let i = 0; i < completed.length; i++)
 	completed[i].style.display = 'none'
 	let added = document.querySelectorAll(".added")
 	for (let i = 0; i < added.length; i++)
-	added[i].style.display = "block"
+		added[i].style.display = "block"
+	ifTaskComplete = false
 }
-
+//filter-function to show all tasks
 function showAll() {
 	let li = document.getElementsByTagName('li')
 	for (let i = 0; i < li.length; i++)
 	li[i].style.display = 'block'
-	
+	ifTaskComplete = false
 }
